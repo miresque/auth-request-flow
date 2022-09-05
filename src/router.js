7b69cc21d8357484d255163d7cfe70f3d87f88f2
mirkan-k <mirkilinc@gmail.com>
2022-09-05 16:55:12 +0100

    feat: Login route - provide token through correct login, Profile route - show user details if token is verified

--- src/router.js
+++ src/router.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 
+require('dotenv').config();
+const secretKey = process.env.JWT_SECRET
+
 const router = express.Router();
 
+const invalidCredentialsMsg = "Some of your credentials may be missing or invalid!"
 const mockUser = {
     username: 'authguy',
     password: 'mypassword',
@@ -13,12 +17,35 @@ const mockUser = {
     }
 };
 
-router.post('/login', (req, res) => {
+const detailsIsValid = (user, pass) => {
+    if ( user === mockUser.username &&
+        pass === mockUser.password) {
+        return true
+    } else {
+        return false
+    }
+}
 
+router.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    if (!detailsIsValid(username, password)) {
+        return res.status(401).json({ error: invalidCredentialsMsg });
+    }
+    
+    const token = jwt.sign({ username, password }, secretKey);
+    res.status(201).json({ token });
 });
 
 router.get('/profile', (req, res) => {
-  
+    const autho = req.get('authorization').split(' ');
+
+    try {
+        jwt.verify(autho[1], secretKey);
+        res.status(200).json({ profile: mockUser });
+    } catch (e) {
+        res.status(401).json({ error: e.message });
+    }
 });
 
 
